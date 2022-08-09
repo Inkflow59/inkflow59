3439a3885e3c50ce52147c7b84c9433bab9db923
Inkflow <101215221+Inkflow59@users.noreply.github.com>
2022-08-09 09:30:59 +0200

    Just for fun

---
+++ ForFun/inkflow.py
@@ -0,0 +1,10 @@
+def Inkflow(number):
+    if number==69:
+        print ("This is absolutely incredible")
+        return None
+    if number==29:
+        print("Love this number !")
+        return None
+    if number==-50:
+        print("This is my IQ !")
+        return None
